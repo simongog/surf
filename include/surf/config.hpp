8ee9ed64ad413a163339ffeef4f4eeda8570a492
Simon Gog <simon.gog@gmail.com>
2014-02-21 10:31:15 +1100

    Added config.

---
+++ include/surf/config.hpp
@@ -0,0 +1,18 @@
+#ifndef SURF_CONFIG_HPP
+#define SURF_CONFIG_HPP
+
+#include <string>
+
+const std::string KEY_DOCWEIGHT = "docweights";
+const std::string KEY_DARRAY = "darray";
+const std::string KEY_DOCPERM = "docperm";
+const std::string KEY_SADADF = "sadadf";
+const std::string KEY_SADADFSEL = "sadadfsel";
+const std::string KEY_WTD = "wtd";
+const std::string KEY_C = "C";
+const std::string KEY_WTC = "wtc";
+const std::string KEY_TMPCST = "tempcst";
+const std::string KEY_TMPDUP = "tmpdup";
+const std::string KEY_WTDUP  = "wtdup";
+
+#endif
