992f69294ee363ae34eb3e6feb72dbffd3cdfe6d
Simon Gog <simon.gog@gmail.com>
2014-03-10 15:45:56 +1100

    Program now returns int_vector<> info.

--- src/test.cpp
+++ src/test.cpp
@@ -1,12 +1,16 @@
-#include "surf/df_sada.hpp"
-#include "sdsl/construct.hpp"
+#include <iostream>
+#include "sdsl/int_vector_buffer.hpp"
 
-using namespace surf;
 using namespace sdsl;
+using namespace std;
 
-int main(){
-//    csa_wt<wt_int<>> csa;
-    df_sada<> df;
-    cache_config cc(false, "../collections/wikishort/index", "SURF");
-    construct(df, "", cc, 0);
+int main(int argc, char* argv[]){
+    if ( argc < 2 ){
+        cout << "./" << argv[0] << " file" << endl;
+        cout << "file has to contain a serialized sdsl::int_vector<>" << endl;
+        cout << "Program outputs the size of elements and the width per element" << endl;
+        return 1;
+    }
+    int_vector_buffer<> ivb(argv[1]);
+    cout << ivb.size() << " " << (int) ivb.width() << endl;
 }
