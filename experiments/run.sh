cb6dd5014c85d460f39c3943cb2b9396f4abab6f
Simon Gog <simon.gog@gmail.com>
2014-03-29 14:03:28 +1100

    Fixed dir.

--- experiments/run.sh
+++ experiments/run.sh
@@ -5,7 +5,7 @@ cd "${MY_DIR}"
 MY_DIR=`pwd`
 SURF_PATH="$MY_DIR/.."
 
-COLLECTIONS="$SURF_PATH/gov2/"
+COLLECTIONS="$SURF_PATH/collections/gov2/"
 PORT=12345
 
 for col in $COLLECTIONS
@@ -14,16 +14,16 @@ do
     do
         IDXNAME=$(echo $idx | sed 's/.*-\(.*\)/\1/g')
         $idx -c $col -p $PORT &
-        for k in 10 100 1000 1000000000
+        for k in 10 100 1000 
         do
-            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2005-efficiency-1000.qry -k $k >> trec-2005-time-$IDXNAME.csv
-            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2006-efficiency-1000.qry -k $k >> trec-2006-time-$IDXNAME.csv
-            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2005-efficiency-1000.qry -k $k -p >> trec-2005-profile-$IDXNAME.csv
-            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2006-efficiency-1000.qry -k $k -p >> trec-2006-profile-$IDXNAME.csv
-            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2005-efficiency-1000.qry -k $k -a >> trec-2005-and-time-$IDXNAME.csv
-            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2006-efficiency-1000.qry -k $k -a >> trec-2006-and-time-$IDXNAME.csv
-            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2005-efficiency-1000.qry -k $k -a -p >> trec-2005-and-profile-$IDXNAME.csv
-            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2006-efficiency-1000.qry -k $k -a -p >> trec-2006-and-profile-$IDXNAME.csv
+            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2005-efficiency-100.qry -k $k >> trec-2005-time-$IDXNAME.csv
+            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2006-efficiency-100.qry -k $k >> trec-2006-time-$IDXNAME.csv
+            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2005-efficiency-100.qry -k $k -p >> trec-2005-profile-$IDXNAME.csv
+            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2006-efficiency-100.qry -k $k -p >> trec-2006-profile-$IDXNAME.csv
+            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2005-efficiency-100.qry -k $k -a >> trec-2005-and-time-$IDXNAME.csv
+            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2006-efficiency-100.qry -k $k -a >> trec-2006-and-time-$IDXNAME.csv
+            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2005-efficiency-100.qry -k $k -a -p >> trec-2005-and-profile-$IDXNAME.csv
+            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2006-efficiency-100.qry -k $k -a -p >> trec-2006-and-profile-$IDXNAME.csv
         done
         # shut down daemon
         $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/wiki.q -k 1 -s > /dev/null
