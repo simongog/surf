ca719f261a3c89e745394b1bfbfef3271bd69c56
Simon Gog <simon.gog@gmail.com>
2014-03-29 13:47:58 +1100

    Get rid of absolut path

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+CUR_DIR=`pwd`
+MY_DIR="$( cd "$( dirname "$0" )" && pwd )" # gets the directory where the script is located in
+cd "${MY_DIR}"
+MY_DIR=`pwd`
+SURF_PATH="$MY_DIR/.."
 
-COLLECTIONS="/devhome3/sgog/ESA2014/surf/collections/gov2/"
-SURF_PATH="/devhome3/sgog/ESA2014/surf"
+COLLECTIONS="$SURF_PATH/gov2/"
 PORT=12345
 
 for col in $COLLECTIONS
@@ -24,3 +29,5 @@ do
         $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/wiki.q -k 1 -s > /dev/null
     done
 done
+
+cd "${CUR_DIR}"
