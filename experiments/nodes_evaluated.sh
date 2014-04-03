0d66ea723f9dd1930e53559ec3b34c45b651bdd0
Simon Gog <simon.gog@gmail.com>
2014-04-03 16:36:34 +1100

    Works on turpin now.

--- experiments/nodes_evaluated.sh
+++ experiments/nodes_evaluated.sh
@@ -3,7 +3,8 @@ CUR_DIR=`pwd`
 MY_DIR="$( cd "$( dirname "$0" )" && pwd )" # gets the directory where the script is located in
 cd "${MY_DIR}"
 MY_DIR=`pwd`
-SURF_PATH=$MY_DIR/..
+#SURF_PATH=$MY_DIR/..
+SURF_PATH=/scratch/VR0052/ESA2014/surf
 
 COLLECTIONS="$SURF_PATH/collections/gov2 $SURF_PATH/collections/cluewebB"
 EXP_DIR="$SURF_PATH/experiments"
@@ -17,14 +18,14 @@ for col in $COLLECTIONS
 do
     for idx in $INDEXES
     do
-        $SURF_PATH/build/surf_daemon-$idx -c $col -p $PORT &
+        $SURF_PATH/build_turpin/surf_daemon-$idx -c $col -p $PORT &
         for k in 10 100 1000 
         do
-            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2005-efficiency-1000.qry -k $k -r 1 -p >> $EXP_DIR/nodes_evaluated_2005.csv
-            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2006-efficiency-1000.qry -k $k -r 1 -p >> $EXP_DIR/nodes_evaluated_2006.csv
+            $SURF_PATH/build_turpin/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2005-efficiency-1000.qry -k $k -r 1 -p >> $EXP_DIR/nodes_evaluated_2005.csv
+            $SURF_PATH/build_turpin/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2006-efficiency-1000.qry -k $k -r 1 -p >> $EXP_DIR/nodes_evaluated_2006.csv
 		done
         # shut down daemon
-        $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/wiki.q -k 1 -s > /dev/null
+        $SURF_PATH/build_turpin/surf_query -h localhost:$PORT -q $SURF_PATH/queries/wiki.q -k 1 -s > /dev/null
     done
 done
 
