33402abd8b665d948f45ae7ed0431cbf01ffa158
Simon Gog <simon.gog@gmail.com>
2014-04-03 16:24:57 +1100

    Adapted nodes_evaluated script.

--- experiments/nodes_evaluated.sh
+++ experiments/nodes_evaluated.sh
@@ -5,12 +5,11 @@ cd "${MY_DIR}"
 MY_DIR=`pwd`
 SURF_PATH=$MY_DIR/..
 
-COLLECTIONS="$SURF_PATH/collections/gov2"
-#COLLECTIONS="$SURF_PATH/collections/trec8"
+COLLECTIONS="$SURF_PATH/collections/gov2 $SURF_PATH/collections/cluewebB"
 EXP_DIR="$SURF_PATH/experiments"
 PORT=12345
 
-INDEXES="IDX_DR IDX_DR_SANSLEN IDX_D IDX_D_SANSLEN"
+INDEXES="IDX_DR IDX_D IDX_D_SANSLEN"
 
 echo "qryid;collection;index;qrymode;k;qrylen;res_size;qry_time;search_time;nodes_evaluated;nodes_total;postings_evaluated;postings_total;client_time" > $EXP_DIR/nodes_evaluated.csv
 
@@ -21,7 +20,8 @@ do
         $SURF_PATH/build/surf_daemon-$idx -c $col -p $PORT &
         for k in 10 100 1000 
         do
-            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2005-efficiency-1000.qry -k $k -r 1 -p >> $EXP_DIR/nodes_evaluated.csv
+            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2005-efficiency-1000.qry -k $k -r 1 -p >> $EXP_DIR/nodes_evaluated_2005.csv
+            $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/trec2006-efficiency-1000.qry -k $k -r 1 -p >> $EXP_DIR/nodes_evaluated_2006.csv
 		done
         # shut down daemon
         $SURF_PATH/build/surf_query -h localhost:$PORT -q $SURF_PATH/queries/wiki.q -k 1 -s > /dev/null
