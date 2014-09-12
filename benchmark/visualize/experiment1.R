b94d1fc48f6fff166164e54d07cae38690d3e795
Simon Gog <simon.gog@gmail.com>
2014-09-12 20:17:10 +0200

    Experiment 1

--- benchmark/visualize/experiment1.R
+++ benchmark/visualize/experiment1.R
@@ -11,12 +11,13 @@ my_format2 <- function(...){
    format(..., nsmall=2,digits=2, big.mark=",")
 }
 
-#multi_figure_style( length(d)/2+1, 2 )  
-#multi_figure_style(1, 1)    
+tikz("experiment1.tex", width = 6.3, height = 3, standAlone = F)
 
 par(mfrow=c(1,2))
+par(las=1)
+par(yaxs="i")
 
-raw[["total_time"]] <- raw[["time_per_query"]]#*raw[["queries"]]
+raw[["total_time"]] <- raw[["time_per_query"]]
 raw2 <- split(raw, raw$multi_occ)
 
 time_all <- raw2[["0"]][["total_time"]] 
@@ -24,163 +25,75 @@ time_ex_singltons <- raw2[["1"]][["total_time"]]
 match_time <- rep(raw2[["0"]][["total_time"]][[1]], length(time_all))
 multi_time <- time_ex_singltons - match_time
 singlton_time <- time_all - multi_time - match_time
-
-
+ks <- raw2[["0"]][["k"]][-1]
 
 data <- rbind(match_time, multi_time, singlton_time)[,-1]
 
+csa_match_col <- "gray10"
+k2_treap_col <- "gray30"
+rmq_csa_col <- "gray80"
 
-partcol <- c("gray10", "gray30", "gray80")
+partcol <- c(csa_match_col, k2_treap_col, rmq_csa_col)
 
 barplot(data, ylab="Avg time per query [$\\mu$secs]",
-        xlab="k", names.arg=seq(1,ncol(data)), col=partcol,
-        main=" |P|=5")
+        xlab="k", names.arg= ks,  col=partcol, cex.axis=0.8, cex.names=0.8)
 
 legend("topleft", legend=rev(c("CSA matching", "$K^2$-treap search","RMQC + CSA locate")), fill=rev(partcol),
-        bty="n")
+        bty="n", cex=0.9)
 
-single_occ <-  (raw2[["0"]][["check_sum"]]-raw2[["1"]][["check_sum"]])[-1]
+all_occ    <-  (raw2[["0"]][["check_sum"]])[-1]
 multi_occ  <-  (raw2[["1"]][["check_sum"]])[-1]
+single_occ <-  all_occ - multi_occ 
 
-multi_occ_time <- (raw2[["1"]][["time_per_query"]]-rep(raw2[["1"]][["time_per_query"]][[1]],length(raw2[["1"]])))[-1]*raw2[["1"]][["queries"]][-1]
-all_time <- (raw2[["0"]][["time_per_query"]]-rep(raw2[["0"]][["time_per_query"]][[1]],length(raw2[["0"]])) )[-1]*raw2[["1"]][["queries"]][-1]
+queries1 <- raw2[["1"]][["queries"]][-1]
+csa1_time <- rep(raw2[["1"]][["time_per_query"]][[1]],nrow(raw2[["1"]])-1)*queries1
+multi_occ_time <- (raw2[["1"]][["time_per_query"]])[-1]*queries1
+multi_occ_ex_csa_time <- multi_occ_time-csa1_time
+avg_multi_occ_time <- multi_occ_time/multi_occ
+avg_multi_occ_ex_csa_time <- multi_occ_ex_csa_time/multi_occ
+avg_multi_occ_csa_time <- avg_multi_occ_time - avg_multi_occ_ex_csa_time
+
+
+queries0 <- raw2[["0"]][["queries"]][-1]
+csa0_time <- rep(raw2[["0"]][["time_per_query"]][[1]],nrow(raw2[["0"]])-1)*queries0
+all_ex_csa_time <- (raw2[["0"]][["time_per_query"]])[-1]-csa0_time
+all_time <- (raw2[["0"]][["time_per_query"]])[-1]*queries0
+
 single_occ_time <- all_time - multi_occ_time
+avg_single_occ_time <- single_occ_time/single_occ
+single_occ_ex_csa_time <- all_ex_csa_time - multi_occ_ex_csa_time 
+avg_single_occ_ex_csa_time <- single_occ_ex_csa_time/single_occ
+avg_single_occ_csa_time <- avg_single_occ_time-avg_single_occ_ex_csa_time
 
-time_per_doc <- raw2[["0"]][["time_per_doc"]][-1]
-plot(time_per_doc, xlab="k", ylab="Avg time per document [$\\mu$secs]",
-     ylim=c(1,40))
+avg_single_occ_ex_csa_time <- avg_single_occ_time - (csa0_time-csa1_time)/single_occ
 
-#    legend( "top", legend=idx_config[idx_ids,"LATEX-NAME"], pch=config[idx_ids,"PCH"], col=config[idx_ids,"COL"],
-#		    lty=config[idx_ids,"LTY"], bty="n", y.intersp=1.5, ncol=2, title="Index", cex=1.2)
+time_per_doc <- raw2[["0"]][["time_per_query"]][-1]*queries0/all_occ
 
+plot(NA, xlab="k", ylab="Avg time per document [$\\mu$secs]",
+     ylim=c(1,1000), xlim=c(0.5, max(length(ks))), xaxt='n', log="y", bty="n")
+abline(h=c(1,2,3,4,5,8,7,8,9,10,20,30,40,50,60,70,80,90,100), col="gray90", lwd=0.8)
 
-#barplot(raw2[["1"]][["total_time"]])
+axis(1, at=seq(1,length(ks)), labels=as.character(ks), tick=F, cex.axis=0.8)
 
-#count <- 0
-#nr <- 0
-#xlabnr <- (2*(length(d)/2)-1) 
-##
-##for( query_len in names(d) ){        
-#
-#    plot( c(1e-9), c(1e-0), xlim=c(min(raw["k"]),max(raw["k"])),
-#                    ylim=c(1,1000),
-#                    ylab="", xlab="", xaxt="n", log="" )
-#    box(col="gray")
-#    abline(h=c(seq(1,10)*0.1,seq(2,10)*1,seq(2,10)*10, seq(2,10)*100), col="lightgray")
-#    grid(lty="solid")
-#    if ( nr %% 2 == 0 ){
-#      ylable <- "Time per query ({\\mu}s)" 
-#      axis( 2, at = axTicks(2) )
-#      mtext(ylable, side=2, line=2, las=0)
-#    }
-#    axis( 1, at = axTicks(1), labels=(nr>=xlabnr)  )
-#    if ( nr >= xlabnr ){
-#      xlable <- "k"
-#      mtext(xlable, side=1, line=2, las=0)
-#    }
-##   Split by IDX_ID
-#    dd <- split(d[[query_len]],d[[query_len]]["index_name"])
-#    for( idx_id in names(dd) ){
-#        ddd <- dd[[idx_id]]
-#        ddd <- subset(ddd, ddd[["TLE"]]==0)
-#        lines(ddd[["k"]], ddd[["time_per_query"]],
-#              lwd=1, type="b", pch=config[idx_id, "PCH"], 
-#			  lty=config[idx_id, "LTY"],
-#		  col=config[idx_id, "COL"])
-#        cat(((ddd[["time_per_query"]]*ddd[["queries"]])-ddd[["time_per_query"]][1])/ddd[["check_sum"]])
-#        cat("\n")
-#    }
-#
-##    draw_figure_heading( sprintf("instance = \\textsc{%s}",tc_config[tc_id,"LATEX-NAME"]) )
-#    draw_figure_heading( sprintf("query length = \\textsc{%s}",query_len) )
-#
-#    nr <- nr+1
-#
-#   if ( nr == 1 ){ # plot legend
-#    plot(NA, NA, xlim=c(0,1),ylim=c(0,1),ylab="", xlab="", bty="n", type="n", yaxt="n", xaxt="n")
-#	idx_ids <- as.character(unique(raw[["IDX_ID"]]))
-#    legend( "top", legend=idx_config[idx_ids,"LATEX-NAME"], pch=config[idx_ids,"PCH"], col=config[idx_ids,"COL"],
-#		    lty=config[idx_ids,"LTY"], bty="n", y.intersp=1.5, ncol=2, title="Index", cex=1.2)
-#    nr <- nr+1
-#  }
-#
-#}
-#
-#dev.off()
-#
-#sink(f_tbl_indexes)
-#cat(typeInfoTable(f_idxconfig, config[["IDX_ID"]], 1, 3, 2))
-#sink(NULL)
-#
-#sink(f_tbl_sizes)
-#raw <- data_frame_from_key_value_pairs(f_sizes)
-## Filter indexes
-#raw               <- raw[raw[["IDX_ID"]]%in%config[["IDX_ID"]],] 
-#raw[["IDX_ID"]]   <- factor(raw[["IDX_ID"]])
-#
-#d <- split(raw,raw[["IDX_ID"]])
-#
-#cat(paste("\\begin{tabular}{@{}l*{",length(names(d)) ,"}{rr@{\ }l}@{}}",sep=""))
-#cat("\\toprule\n")
-#cat(paste("Collection& & \\multicolumn{",length(names(d))*3-1,"}{c}{Index size in MiB (fraction of original collection)}\\\\\n",sep=""))
-#cat(paste("\\cmidrule{1-1}\\cmidrule{3-",length(names(d))*3+1,"}\n",sep=""))
-#for( idx_id in names(d) ){        
-#    cat(paste("&\\ &\\multicolumn{2}{c}{",idx_config[idx_id,"LATEX-NAME"],"}",sep=""))
-#}
-#cat("\\\\[2ex]\n")
-#
-#dd <- split(raw,raw[["TC_ID"]])
-#
-#for ( tc_id in names(dd) ){
-#    cat("{\\sc ",tc_config[tc_id,"LATEX-NAME"],"}")
-#    ddd <- split(dd[[tc_id]],dd[[tc_id]][["IDX_ID"]])
-#    for ( idx_id in names(d) ){
-#        row <- ddd[[idx_id]]
-#        cat(paste("&&",my_format2(row["size"]/1024**2),"&",sprintf("(%.2f)",row["size"]/row["text_size"])))
-#    }
-#    cat("\\\\\n")
-#}
-#cat("\\bottomrule\n")
-#cat("\\end{tabular}")
-#
-#sink(NULL)
-#
-#sink(f_tbl_collections)
-#raw <- data_frame_from_key_value_pairs(f_results)
-#
-#cat(paste("\\begin{tabular}{@{}l*{5}{r}@{}}\n",sep=""))
-#cat("\\toprule\n")
-#elem = "Characters"
-#if ( suf=="_int" ) {
-#    elem = "Words"
-#}
-#cat(paste("Collection & ",elem," & Documents & ","Avg. doc. len.& gzip-compr.& xz-compr.\\\\[1ex]\n",sep=""))
-#
-#d <- split(raw,raw["TC_ID"])
-#for ( tc_id in names(d) ){
-#    zfile = paste("../",tc_config[tc_id,"PATH"],".z.info",sep="")
-#    gzipcomp = NA
-#    xzcomp   = NA
-#    if ( file.exists(zfile) ){
-#        comp_info <- readConfig(zfile,c("NAME","RATIO","COMMAND"))
-#        gzipcomp = my_format2(comp_info["gzip","RATIO"])
-#        xzcomp   = my_format2(comp_info["xz","RATIO"])
-#    }
-#    doc_cnt_i  <- unique(d[[tc_id]][["doc_cnt"]])
-#    word_cnt_i <- unique(d[[tc_id]][["word_cnt"]])
-#    doc_cnt    <- format(doc_cnt_i, big.mark=",")
-#    word_cnt   <- format(word_cnt_i, big.mark=",")
-#    avg_doc_len <- my_format2(word_cnt_i/doc_cnt_i)
-#    cat("{\\sc ",tc_config[tc_id,"LATEX-NAME"],"}")
-#    cat("&",word_cnt,"&",doc_cnt,"&",avg_doc_len,"&",
-#        gzipcomp, "&", xzcomp,"\\\\\n")
-#}
-#cat("\\bottomrule\n")
-#cat("\\end{tabular}")
-#sink(NULL)
-#
-#}
-#
-#
-#process_data("")
-#process_data("_intFaster and Smaller Inverted Indices with Treaps.")
+mixed_col = "black"
+
+legend("topright",
+        legend=c("$K^2$-treap retrieved","mixed","RMQC + CSA retrieved"),
+        fill=c(k2_treap_col, mixed_col, rmq_csa_col),
+        bty="n", cex=0.9
+        )
+
+for ( i in 1:nrow(raw2[["0"]][-1,]) ){
+
+    lines( rep(i,2)-0.15,
+           c(0.001, avg_multi_occ_time[i]),
+           lwd=5, col=k2_treap_col, lend=1 ) 
+    lines( rep(i,2),
+           c(0.001, time_per_doc[i]),
+           lwd=5, col=mixed_col, lend=1 ) 
+    lines( rep(i,2)+0.15,
+           c(0.001, avg_single_occ_time[i]),
+           lwd=5, col=rmq_csa_col, lend=1 ) 
+}
+
+dev.off()
