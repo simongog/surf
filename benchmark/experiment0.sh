039a6c13f21b30b4730a1d85fa3aa7ba193e018c
Simon Gog <simon.gog@gmail.com>
2014-09-15 11:05:14 +0200

    Added index identifier

--- benchmark/experiment0.sh
+++ benchmark/experiment0.sh
@@ -9,9 +9,11 @@ for f in ${info}; do
     suf=${f#../collections/}
     col=${suf%/index/*}
     col_dir=${f%/index/*}
+    idx=${suf#*/index/space_usage_}
+    idx=${idx%.html}
     col_size=`wc -c < ${col_dir}/text*_SURF.sdsl | tr -d ' '`
     size_info=`../build/size_info < $f`
     for line in ${size_info}; do
-        echo "${line};${col};${col_size}" >> ${res}
+        echo "${line};${col};${col_size};${idx}" >> ${res}
     done
 done
