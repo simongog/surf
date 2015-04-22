e9cb6f624e72c41bc53dc59b13ea11fe98048440
Matthias Stumpp <mstumpp@gmail.com>
2015-04-23 00:31:46 +0200

    modified surf_daemon to accept/return json-formatted request/response

--- src/surf_daemon.cpp
+++ src/surf_daemon.cpp
@@ -17,6 +17,7 @@
 #include "surf/rank_functions.hpp"
 
 #include "zmq.hpp"
+#include <json/json.h>
 
 typedef struct cmdargs {
     std::string collection_dir;
@@ -102,48 +103,67 @@ int main(int argc,char* const argv[])
 
     /* daemon mode */
     {
-    	std::cout << "Starting daemon mode on port " << args.port << std::endl;
-    	zmq::context_t context(1);
-    	zmq::socket_t server(context, ZMQ_REP);
-    	server.bind(std::string("tcp://*:"+args.port).c_str());
+        std::cout << "Starting daemon mode on port " << args.port << std::endl;
+        zmq::context_t context(1);
+        zmq::socket_t server(context, ZMQ_REP);
+        server.bind(std::string("tcp://*:"+args.port).c_str());
 
-    	while(true) {
-    		zmq::message_t request;
-    		/* wait for msg */
-    		server.recv(&request);
-            surf_qry_request* surf_req = (surf_qry_request*) request.data();
+        while(true) {
+            zmq::message_t request;
+            /* wait for msg */
+            server.recv(&request);
 
-            if(surf_req->type == REQ_TYPE_QUIT) {
+            std::string rpl = std::string(static_cast<char*>(request.data()), request.size());
+            std::cout << rpl << std::endl;
+
+            Json::Value root;
+            Json::Reader reader;
+
+            bool parsingSuccessful = reader.parse(rpl, root);
+            if(!parsingSuccessful) {
+                std::cout << "ERROR IN JSON PARSING PROCESS. SKIPPING QUERY" << reader.getFormatedErrorMessages();
+                return 16;
+            }
+
+            surf_qry_request surf_req;
+            surf_req.type = root.get("type", 0).asInt();
+            surf_req.mode = root.get("mode", 0).asInt();
+            surf_req.phrases = root.get("phrases", 0).asInt();
+            surf_req.phrase_threshold = root.get("phrase_threshold", 0.0).asDouble();
+            surf_req.id = root.get("id", 0).asUInt64();
+            surf_req.k = root.get("k", 0).asUInt64();
+            surf_req.output_results = root.get("output_results", 0).asInt();
+            surf_req.int_qry = root.get("int_qry", 0).asInt();
+            std::ostringstream qry_str_oss;
+            qry_str_oss << surf_req.id << ";" << root.get("qry_str", "").asString();
+            std::string qry_str = qry_str_oss.str();
+
+            if(surf_req.type == REQ_TYPE_QUIT) {
                 std::cout << "Quitting..." << std::endl;
                 break;
             }
 
-    		/* perform query */
-    		auto qry_start = clock::now();
+            /* perform query */
+            auto qry_start = clock::now();
 
             surf::query_t prased_query;
             bool parse_ok = false;
 
-            if(surf_req->phrases) { 
+            if(surf_req.phrases) { 
 #ifdef PHRASE_SUPPORT
                 const auto& id_mapping = term_map.first;
                 const auto& reverse_mapping = term_map.second;
-                auto qry_mapping = surf::query_parser::map_to_ids(id_mapping,
-                                            std::string(surf_req->qry_str),true,surf_req->int_qry);
+                auto qry_mapping = surf::query_parser::map_to_ids(id_mapping,qry_str,true,surf_req.int_qry);
                 if(std::get<0>(qry_mapping)) {
                     auto qid = std::get<1>(qry_mapping);
                     auto qry_ids = std::get<2>(qry_mapping);
-                    prased_query = surf::phrase_parser::phrase_segmentation(index.m_csa,qry_ids,reverse_mapping,
-                                                                           surf_req->phrase_threshold);
+                    prased_query = surf::phrase_parser::phrase_segmentation(index.m_csa,qry_ids,reverse_mapping, surf_req.phrase_threshold);
                     std::get<0>(prased_query) = qid;
                     parse_ok = true;
                 }
 #endif
             } else {
-                auto qry = surf::query_parser::parse_query(term_map,
-                                            std::string(surf_req->qry_str),
-                                            true,
-                                            surf_req->int_qry);
+                auto qry = surf::query_parser::parse_query(term_map,qry_str,true,surf_req.int_qry);
                 if(qry.first) {
                     prased_query = qry.second;
                     parse_ok = true;
@@ -154,7 +174,7 @@ int main(int argc,char* const argv[])
                 // error parsing the qry. send back error
                 surf_time_resp surf_resp;
                 surf_resp.status = REQ_PARSE_ERROR;
-                surf_resp.req_id = surf_req->id;
+                surf_resp.req_id = surf_req.id;
                 zmq::message_t reply (sizeof(surf_time_resp));
                 memcpy(reply.data(),&surf_resp,sizeof(surf_time_resp));
                 server.send(reply);
@@ -162,34 +182,34 @@ int main(int argc,char* const argv[])
                 continue;
             }
 
-    		/* (1) parse qry terms */
+            /* (1) parse qry terms */
             bool profile = false;
-            if(surf_req->mode == REQ_MODE_PROFILE) {
+            if(surf_req.mode == REQ_MODE_PROFILE) {
                 profile = true;
             }
             bool ranked_and = false;
-            if(surf_req->type == REQ_TYPE_QRY_AND) {
+            if(surf_req.type == REQ_TYPE_QRY_AND) {
                 ranked_and = true;
             }
 
-    		/* (2) query the index */
+            /* (2) query the index */
             auto qry_id = std::get<0>(prased_query);
             auto qry_tokens = std::get<1>(prased_query);
             auto search_start = clock::now();
-            auto results = index.search(qry_tokens,surf_req->k,ranked_and,profile);
+            auto results = index.search(qry_tokens,surf_req.k,ranked_and,profile);
             auto search_stop = clock::now();
             auto search_time = std::chrono::duration_cast<std::chrono::microseconds>(search_stop-search_start);
 
-    		auto qry_stop = clock::now();
-    		auto query_time = std::chrono::duration_cast<std::chrono::microseconds>(qry_stop-qry_start);
+            auto qry_stop = clock::now();
+            auto query_time = std::chrono::duration_cast<std::chrono::microseconds>(qry_stop-qry_start);
 
             /* (3a) output to qry to console */
-            std::cout << "REQ=" << std::left << std::setw(10) << surf_req->id << " " 
-                      << " k="  << std::setw(5) << surf_req->k 
+            std::cout << "REQ=" << std::left << std::setw(10) << surf_req.id << " " 
+                      << " k="  << std::setw(5) << surf_req.k 
                       << " QID=" << std::setw(5) << qry_id 
                       << " TIME=" << std::setw(7) << query_time.count()/1000.0
                       << " AND=" << ranked_and
-                      << " PHRASE=" << surf_req->phrases;
+                      << " PHRASE=" << surf_req.phrases;
             std::cout << " [";
             if(args.load_dictionary) {
                 for(const auto& token : qry_tokens) {
@@ -220,42 +240,60 @@ int main(int argc,char* const argv[])
             }
             std::cout << "]" << std::endl;
 
-    		/* (3) create answer and send */
-            if(!surf_req->output_results) {
-                surf_time_resp surf_resp;
-                surf_resp.status = REQ_RESPONE_OK;
-                strncpy(surf_resp.index,index_name.c_str(),sizeof(surf_resp.index));
-                strncpy(surf_resp.collection,base_name.c_str(),sizeof(surf_resp.collection));
-                strncpy(surf_resp.ranker,surf_index_t::ranker_type::name().c_str(),sizeof(surf_resp.ranker));
-                surf_resp.req_id = surf_req->id;
-                surf_resp.k = surf_req->k;
-                surf_resp.qry_id = qry_id;
-                surf_resp.qry_len = qry_tokens.size();
-                surf_resp.result_size = results.list.size();
-                surf_resp.qry_time = query_time.count();
-                surf_resp.search_time = search_time.count();
-                surf_resp.wt_search_space = results.wt_search_space;
-                surf_resp.wt_nodes = results.wt_nodes;
-                surf_resp.postings_evaluated = results.postings_evaluated;
-                surf_resp.postings_total = results.postings_total;
+            /* (3) create answer and send */
+            if(!surf_req.output_results) {
 
-        		zmq::message_t reply (sizeof(surf_time_resp));
-        		memcpy(reply.data(),&surf_resp,sizeof(surf_time_resp));
-        		server.send (reply);
+                Json::Value res;
+                res['status'] = REQ_RESPONE_OK;
+                res['index'] = index_name.c_str();
+                res['collection'] = base_name.c_str();
+                res['ranker'] = surf_index_t::ranker_type::name().c_str();
+                res['req_id'] = surf_req.id;
+                res['k'] = surf_req.k;
+                res['qry_id'] = qry_id;
+                //res['qry_len'] = qry_tokens.size();
+                //res['result_size'] = results.list.size();
+                res['qry_time'] = query_time.count();
+                res['search_time'] = search_time.count();
+                res['wt_search_space'] = results.wt_search_space;
+                res['wt_nodes'] = results.wt_nodes;
+                res['postings_evaluated'] = results.postings_evaluated;
+                res['postings_total'] = results.postings_total;
+
+                Json::StyledWriter writer;
+                std::string res_str = writer.write(res);
+
+                std::cout << res_str << std::endl;
+
+                zmq::message_t reply(res_str.length());
+                memcpy(reply.data(), res_str.c_str(), res_str.length());
+                server.send(reply);
+
             } else {
-                size_t res_size = results.list.size()*2*sizeof(double) + sizeof(uint64_t);
-                zmq::message_t zmq_results (res_size);
-                surf_results* sr = (surf_results*)(zmq_results.data());
-                sr->size = results.list.size();
-                for(size_t i=0;i<results.list.size();i++) {
-                    sr->data[i*2] = results.list[i].doc_id;
-                    sr->data[i*2+1] = results.list[i].score;
-                }
-                server.send (zmq_results);
-            }
-    	}
-    }
 
+                Json::Value res;
+                Json::Value resultsArray;
+
+                for(size_t i=0;i<results.list.size();i++) {
+                    Json::Value result;
+                    result["doc_id"] = results.list[i].doc_id;
+                    result["score"] = results.list[i].score;
+                    resultsArray.append(result);
+                }
+
+                res["results"] = resultsArray;
+                
+                Json::StyledWriter writer;
+                std::string res_str = writer.write(res);
+
+                std::cout << res_str << std::endl;
+
+                zmq::message_t reply(res_str.length());
+                memcpy(reply.data(), res_str.c_str(), res_str.length());
+                server.send(reply);
+            }
+        }
+    }
 
     return EXIT_SUCCESS;
 }
